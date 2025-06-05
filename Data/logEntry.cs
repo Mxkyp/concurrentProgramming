9fbcda28943cfadc96f75c17b5e57f1384461e67
Mxkcpy <mikolaj19@poczta.fm>
2025-06-05 07:25:13 +0200

    created BallCollisionLogEntry

--- Data/logEntry.cs
+++ Data/logEntry.cs
@@ -11,13 +11,13 @@
 
   internal class LogEntry : ILogEntry 
   {
-    internal LogEntry(DateTime timeStamp, int threadId, string message, IVector position, IVector velocity)
+    internal LogEntry(DateTime timeStamp, Guid ballId, string message, IVector position, IVector velocity)
     {
       Timestamp = timeStamp;
-      BallId = threadId;
+      BallId = ballId;
       Message = message;
       Position = position;
-      Velocity = velocity;
+      Velocity = velocity; 
     }
 
     public DateTime Timestamp { get; init; }
@@ -25,5 +25,29 @@
     public string Message { get; init; }
     public IVector Position { get; init; }
     public IVector Velocity { get; init; }
+  }
+  internal class BallCollisionLogEntry : ILogEntry 
+  {
+
+    internal BallCollisionLogEntry(DateTime timeStamp, Guid ballId, string message, IVector position, IVector velocity, Guid ballId2, IVector position2, IVector velocity2)
+    {
+      Timestamp = timeStamp;
+      BallId = ballId;
+      Message = message;
+      Position = position;
+      Velocity = velocity;
+      BallId2 = ballId2;
+      Position2 = position2;
+      Velocity2 = velocity2;
+    }
+
+    public DateTime Timestamp { get; init; }
+    public Guid BallId { get; init; }
+    public string Message { get; init; }
+    public IVector Position { get; init; }
+    public IVector Velocity { get; init; }
+    public Guid BallId2 { get; init; }
+    public IVector Position2 { get; init; }
+    public IVector Velocity2 { get; init; }
   }
 }
