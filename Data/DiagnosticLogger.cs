828b4d1d580389955ea8a68b19ddf25f18c46f01
Mxkcpy <mikolaj19@poczta.fm>
2025-06-05 07:29:13 +0200

    removing diagnosticBuffer

--- Data/DiagnosticLogger.cs
+++ Data/DiagnosticLogger.cs
@@ -1,10 +1,11 @@
 ﻿using System.Text.Json;
+using System.Collections.Concurrent;
 
 namespace TP.ConcurrentProgramming.Data
 {
   internal class DiagnosticLogger : ILogger
   {
-    private readonly DiagnosticBuffer buffer;
+   private readonly BlockingCollection<ILogEntry> buffer;
     private readonly Thread writerThread;
     private readonly string filePath;
     internal DiagnosticLogger(string relativePath = @"..\..\..\..\logs\diagnostic.json", int bufferCapacity = 500)
@@ -12,18 +13,13 @@ namespace TP.ConcurrentProgramming.Data
       filePath = Path.GetFullPath(relativePath); 
       Directory.CreateDirectory(Path.GetDirectoryName(filePath)!); 
 
-      buffer = new DiagnosticBuffer(bufferCapacity);
+      buffer = new BlockingCollection<ILogEntry>(bufferCapacity);
       writerThread = new Thread(WriteLoop);
       writerThread.Start();
     }
-    public void Log(string timestamp, int threadId, string message, IVector position, IVector velocity)
+    public void Log(DateTime timestamp, Guid ballId, string message, IVector position, IVector velocity)
     {
-      buffer.TryAdd(new LogEntry(timestamp, threadId,  message, position, velocity));
-    }
-
-    public void Stop()
-    {
-      buffer.Stop();
+      buffer.TryAdd(new LogEntry(timestamp, ballId,  message, position, velocity));
     }
 
     private void WriteLoop()
@@ -31,11 +27,26 @@ namespace TP.ConcurrentProgramming.Data
       using StreamWriter writer = new StreamWriter(filePath, append: true);
       while (true)
       {
-        LogEntry? log = buffer.WaitAndTake();
+        LogEntry? log = WaitAndTake();
         if (log == null) { break; }
         writer.WriteLine(JsonSerializer.Serialize(log));
         writer.Flush(); // immediate write (real-time)
       }
     }
+   private ILogEntry? WaitAndTake()
+   {
+      ILogEntry? result = null;
+
+      while (!buffer.IsCompleted)
+      {
+        if(buffer.TryTake(out ILogEntry itemRead, 50))
+        {
+          result = itemRead;
+          break;
+        }
+      }
+
+      return result;
+   }
   }
 }
